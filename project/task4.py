5dcab4e2c84d01263775df76191f5b4a2ccd86bd
IIlya-ipti <zuc.120900@gmail.com>
2024-05-09 12:53:59 +0300

    ready!

--- project/task4.py
+++ project/task4.py
@@ -22,6 +22,7 @@ def diagonalize_ones_matrix(matrix: dok_matrix, m: int) -> dok_matrix:
 def reachability_with_constraints(
     fa: FiniteAutomaton, constraints_fa: FiniteAutomaton
 ) -> dict[int, set[int]]:
+
     main = fa.get_matrix_word()
     constraints = constraints_fa.get_matrix_word()
 
