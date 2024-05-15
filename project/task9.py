4aa3d2ad37f37f254f7d81a839aedf903f07b5ae
IIlya-ipti <zuc.120900@gmail.com>
2024-05-15 22:45:55 +0300

    fix

--- project/task9.py
+++ project/task9.py
@@ -17,11 +17,17 @@ from collections import defaultdict
 from .task7 import cfg_to_weak_normal_form
 import scipy as sp
 from .task8 import *
-from pyformlang.finite_automaton import TransitionFunction, EpsilonNFA, Symbol, State, NondeterministicTransitionFunction
+from pyformlang.finite_automaton import (
+    TransitionFunction,
+    EpsilonNFA,
+    Symbol,
+    State,
+    NondeterministicTransitionFunction,
+)
 from copy import deepcopy
 
 
-def add_tmp_automation(rsm: RecursiveAutomaton, label : str):
+def add_tmp_automation(rsm: RecursiveAutomaton, label: str):
     nfa = ndfa.NondeterministicFiniteAutomaton()
     state0 = State(0)
     state1 = State(1)
@@ -37,12 +43,13 @@ def add_tmp_automation(rsm: RecursiveAutomaton, label : str):
     rsm.add_box(Box(nfa, Symbol(label)))
     return rsm
 
+
 def cfpq_with_gll(
-        cfg_or_rsm: RecursiveAutomaton,
-        graph: nx.DiGraph,
-        start_nodes: set[int] = None,
-        final_nodes: set[int] = None,
-    ) -> set[tuple[int, int]]:
+    cfg_or_rsm: RecursiveAutomaton,
+    graph: nx.DiGraph,
+    start_nodes: set[int] = None,
+    final_nodes: set[int] = None,
+) -> set[tuple[int, int]]:
 
     rsm = (
         cfg_or_rsm
@@ -54,18 +61,17 @@ def cfpq_with_gll(
     final_nodes = graph.nodes if final_nodes is None else final_nodes
 
     new_init_label = "S_____"
-    rsm = add_tmp_automation(rsm,new_init_label)
-    
-    dict_rsm = { i : rsm.boxes[i].dfa.to_dict() for i in rsm.boxes}
-    dict_rsm_start = { i : rsm.boxes[i].dfa.start_states for i in rsm.boxes}
-    dict_rsm_final = { i : rsm.boxes[i].dfa.final_states for i in rsm.boxes}
-    dict_rsm_states = { i : rsm.boxes[i].dfa.states for i in rsm.boxes}
-    
+    rsm = add_tmp_automation(rsm, new_init_label)
 
-    node_to_popped = defaultdict(lambda : set())
+    dict_rsm = {i: rsm.boxes[i].dfa.to_dict() for i in rsm.boxes}
+    dict_rsm_start = {i: rsm.boxes[i].dfa.start_states for i in rsm.boxes}
+    dict_rsm_final = {i: rsm.boxes[i].dfa.final_states for i in rsm.boxes}
+    dict_rsm_states = {i: rsm.boxes[i].dfa.states for i in rsm.boxes}
+
+    node_to_popped = defaultdict(lambda: set())
     node_to_state = {}
     state_to_node = {}
-    gss = defaultdict(lambda : defaultdict(lambda : set()))
+    gss = defaultdict(lambda: defaultdict(lambda: set()))
 
     def clear():
         node_to_popped.clear()
@@ -74,6 +80,7 @@ def cfpq_with_gll(
         gss.clear()
 
     inc = 0
+
     def create_node(symb, graph_state):
         if (symb, graph_state) in state_to_node:
             return state_to_node[(symb, graph_state)]
@@ -86,68 +93,71 @@ def cfpq_with_gll(
 
     def add_to_gss(from_, to_, label):
         gss[from_][to_].add(label)
-    
-    def handle_popped(g_from,g_to, pi):
+
+    def handle_popped(g_from, g_to, pi):
         result = set()
         for i in node_to_popped[g_from]:
-            _,v,_ = i
-            result = result.union(set([(pi, v,g_to)]))
+            _, v, _ = i
+            result = result.union(set([(pi, v, g_to)]))
         return result
 
-
-    def handle(d : tuple):
-        (vi, node ,gk) = d
+    def handle(d: tuple):
+        (vi, node, gk) = d
 
         symb_gk, _ = node_to_state[gk]
 
         nodes = graph.adj[node]
 
-        dict_by_label_graph = defaultdict(lambda : set())
+        dict_by_label_graph = defaultdict(lambda: set())
         for i in nodes:
             for labels in nodes[i]:
-                dict_by_label_graph[nodes[i][labels]['label']].add(i)
-        
-        dict_by_label_rsm = defaultdict(lambda : set())
+                dict_by_label_graph[nodes[i][labels]["label"]].add(i)
+
+        dict_by_label_rsm = defaultdict(lambda: set())
         # todo test by vi
         if vi in dict_rsm[symb_gk]:
             for i in dict_rsm[symb_gk][vi]:
                 to = dict_rsm[symb_gk][vi][i]
                 for labels in to if isinstance(to, set) else {to}:
                     dict_by_label_rsm[i].add(labels)
-        
+
         new_desk = set()
 
         for symb in dict_by_label_rsm:
-            #first
+            # first
             if symb in dict_by_label_graph:
                 for nodej in dict_by_label_graph[symb]:
                     for vj in dict_by_label_rsm[symb]:
-                        new_desk.add((vj,nodej,gk))
+                        new_desk.add((vj, nodej, gk))
             # second
             if symb in dict_rsm:
                 new_g = create_node(symb, node)
                 for pi in dict_by_label_rsm[symb]:
-                    add_to_gss(new_g,gk,pi)
+                    add_to_gss(new_g, gk, pi)
                     new_desk = new_desk.union(handle_popped(new_g, gk, pi))
                 for qs in dict_rsm_start[symb]:
                     new_desk.add((qs, node, new_g))
-                
-            #third
+
+            # third
             # if symb in dict_rsm_final:
         if vi in dict_rsm_final[symb_gk]:
             for g_old in gss[gk]:
                 for pi in gss[gk][g_old]:
-                    new_desk.add((pi,node,g_old))
+                    new_desk.add((pi, node, g_old))
                     node_to_popped[gk].add(d)
 
         return new_desk
-            
+
     result = set()
     for start_node in start_nodes:
         clear()
         Dproc = set()
         for start_rsm_node in dict_rsm_start[new_init_label]:
-            config = (start_rsm_node, start_node, create_node(new_init_label, start_node))
+            config = (
+                start_rsm_node,
+                start_node,
+                create_node(new_init_label, start_node),
+            )
             Dproc.add(config)
 
         Dhandled = set()
@@ -156,13 +166,11 @@ def cfpq_with_gll(
 
             g_tmp = node_to_state[d[2]]
 
-
-            if d[0] == '1' and g_tmp[0] == new_init_label and d[1] in final_nodes:
+            if d[0] == "1" and g_tmp[0] == new_init_label and d[1] in final_nodes:
                 result.add((start_node, d[1]))
 
-
             new_ds = handle(d)
-            
+
             new_ds_tmp = set()
             # filter
             for i in new_ds:
